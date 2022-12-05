7ae3aba355ab70f6a3d44c7bf150fe53f36534f0
AliceTcvetkova <1409alice@gmail.com>
2022-12-05 19:01:27 +0300

    Task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,3 +3,23 @@
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine(a);
+    Console.WriteLine( "больше");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine(a);
+    Console.WriteLine( "меньше");
+    Console.WriteLine(b);
+}
