f5858a568cef32642dce1d848e462940e4499f75
AliceTcvetkova <1409alice@gmail.com>
2022-12-05 18:55:42 +0300

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,3 +2,20 @@
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22 */
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b > max)
+    max = b;
+if (c > max)
+    max = c;
+Console.WriteLine(max);
