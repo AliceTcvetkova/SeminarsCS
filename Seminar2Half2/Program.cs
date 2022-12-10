23169184a8fb34de55214fab45c856069b01ae85
AliceTcvetkova <1409alice@gmail.com>
2022-12-10 23:13:29 +0300

    Task 14

--- Seminar2Half2/Program.cs
+++ Seminar2Half2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int dev1 = number % (7*23);
+if (dev1 == 0)
+    Console.WriteLine("кратно");
+else
+    Console.WriteLine("не кратно");
