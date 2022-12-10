6ab04227111a25388d474e1009b7118d83767d5c
AliceTcvetkova <1409alice@gmail.com>
2022-12-10 22:40:48 +0300

    Added Class Task

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа.
+// 78->8
+// 12->2
+// 85->8
+Console.Clear();
+int number = new Random().Next(10,100);
+Console.WriteLine($"Сгенерированное число: {number}");
+int n1 = number / 10;
+int n2 = number % 10;
+if (n1>n2)
+    Console.WriteLine(n1);
+else
+    Console.WriteLine(n2);
