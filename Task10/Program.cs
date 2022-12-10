cb2565839eb9a021cead972a2a91d59d0e06c675
AliceTcvetkova <1409alice@gmail.com>
2022-12-10 23:50:01 +0300

    10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    int dev = number / 10;
+    int n2 = dev % 10;
+    Console.WriteLine(n2);
+}
+else
+    Console.WriteLine("не трёхзначное число");
