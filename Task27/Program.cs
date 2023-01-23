db4f078b03de294f8271f68e19e2ed7cbf2173ad
AliceTcvetkova <1409alice@gmail.com>
2023-01-24 00:24:56 +0300

    Task 27 is ready'

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int res = 0; 
+while (a > 0)
+{
+    res += a % 10;
+    a /= 10;
+}
+Console.WriteLine(res);
