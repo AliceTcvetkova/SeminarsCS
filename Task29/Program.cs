8cc44e22114282f2d0ce98f75edb4d06ac39aa99
AliceTcvetkova <1409alice@gmail.com>
2023-01-24 00:46:08 +0300

    Task 29 is ready

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая спрашивает у пользователя кол-во элементов массива, 
+// затем сами элементы. Программ должна вывести массив на экран.
+Console.WriteLine("Введите число элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите элементы массива: ");
+string[] array = Console.ReadLine().Split(", ");
+Console.WriteLine($"[{String.Join(", ",array)}]");
