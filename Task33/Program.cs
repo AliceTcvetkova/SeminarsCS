3427535c0f5a2407db34dec987b12433eab0be7d
AliceTcvetkova <1409alice@gmail.com>
2023-02-05 17:26:29 +0300

    Task 33

--- Task33/Program.cs
+++ Task33/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9,10);
+}
+
+string ReleaseArray(int[] array, int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (number == array[i])
+            return "yes";
+    }
+    return "no";
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.Write("Введите число: ");
+int k = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ReleaseArray(array, k));
