215149623b76c95425df034f5001979ccc8f28c0
AliceTcvetkova <1409alice@gmail.com>
2023-02-05 16:48:57 +0300

    Task 31

--- Task31/Program.cs
+++ Task31/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9,9]
+// Найдите сумму отрицательных и положительных элементов массива (отдельно)
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9,10);
+}
+
+void ReleaseArray(int[] array)
+{
+    int sumPositive = 0, sumNegative = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            sumPositive += array[i];
+        else 
+            sumNegative += array[i];
+    }
+    Console.WriteLine($"Сумма положительных чисел равна {sumPositive}");  
+    Console.WriteLine($"Сумма отрицательных чисел равна {sumNegative}"); 
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
