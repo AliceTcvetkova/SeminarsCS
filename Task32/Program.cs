b67b23a4e93cc207e65e7f05111d18a941877c7a
AliceTcvetkova <1409alice@gmail.com>
2023-02-05 17:05:45 +0300

    Task 32

--- Task32/Program.cs
+++ Task32/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9,10);
+}
+
+void ReleaseArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] *= (-1);
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ReleaseArray(array);
+Console.WriteLine($"Конечный массив:[{string.Join(", ", array)}]");
