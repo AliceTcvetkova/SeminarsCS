fdf08ac37093dbeeeca88c1bc1adc32deb4c68b5
AliceTcvetkova <1409alice@gmail.com>
2023-02-09 19:58:07 +0300

    Task 39

--- Task39/Program.cs
+++ Task39/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая перевернёт одномерный массив 
+// последний элемент на первом месте, а первый на последнем)
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1,11);
+}
+
+void ReleaseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+Console.Clear();
+Console.WriteLine("Сколько элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ReleaseArray(array);
+Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
