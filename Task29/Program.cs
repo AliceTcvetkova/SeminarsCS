fd6a7583e7cf5cbc20c4e59b63a648871dc352bd
AliceTcvetkova <1409alice@gmail.com>
2023-02-14 23:06:52 +0300

    29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,7 +1,13 @@
-﻿// Напишите программу, которая спрашивает у пользователя кол-во элементов массива, 
-// затем сами элементы. Программ должна вывести массив на экран.
-Console.WriteLine("Введите число элементов массива: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите элементы массива: ");
-string[] array = Console.ReadLine().Split(", ");
-Console.WriteLine($"[{string.Join(", ",array)}]");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Напишите программу, которая спрашивает у пользователя кол-во элементов массива, 
+        // затем сами элементы. Программ должна вывести массив на экран.
+        Console.WriteLine("Введите число элементов массива: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите элементы массива: ");
+        string[] array = Console.ReadLine().Split(", ");
+        Console.WriteLine($"[{string.Join(", ", array)}]");
+    }
+}
