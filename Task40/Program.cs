1cd60f7bb94b8f3f84c2366bbd30ce721f8893ce
AliceTcvetkova <1409alice@gmail.com>
2023-02-14 22:31:16 +0300

    40

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Проверка сторон треугольника
+
+Console.Write("Введите стороны треугольника: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+if ((n1 + n2 > n3) && (n3 + n2 > n1) && (n1 + n3 > n2))
+    Console.Write("Может");
+else
+    Console.Write("Не может");
